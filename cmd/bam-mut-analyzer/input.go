6e0c52150add6c96d0c1892da18f65600d197e7d
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2026-01-29 14:48:53 +0800

    🐛fix load from excel

--- cmd/bam-mut-analyzer/input.go
+++ cmd/bam-mut-analyzer/input.go
@@ -9,15 +9,23 @@ func readExcelSampleOrder(filePath string) ([]string, error) {
 	}
 
 	var samples []string
+	var cIdx = -1
 	// 假设样本名称在Sheet1的第一列
 	sheet := xlFile.Sheets[0]
 	for i, row := range sheet.Rows {
 		if i == 0 {
 			// 跳过表头
+			for j := range len(row.Cells) {
+				if row.Cells[j].String() == "样品名称" {
+					cIdx = j
+					break
+				}
+
+			}
 			continue
 		}
-		if len(row.Cells) > 0 {
-			sampleName := row.Cells[0].String()
+		if len(row.Cells) > cIdx {
+			sampleName := row.Cells[cIdx].String()
 			if sampleName != "" {
 				samples = append(samples, sampleName)
 			}
